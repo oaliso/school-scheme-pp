a2be042b1d7a976ccb74b62d2706a36a60e12244
oaliso <alissoncsilva0511@gmail.com>
2025-04-21 21:40:03 -0400

    Aluno, Professor & Curso has created

--- Aluno.java
+++ Aluno.java
@@ -1,9 +1,61 @@
+/* * 
+ * Classe desenvolvida por Alisson de Carvalho Silva 
+ * Pendente: assoicação com: turma, relatório
+* */
+
 
 public class Aluno {
 	
 	private String nome;
 	private int id;
 	private String email;
+	
+	
+	// instantiation < --- > constructors
+	
+	
+	public Aluno () {
+		
+	}
+	
+	public Aluno (String nome, int id, String email) {
+		this.nome = nome;
+		this.id = id;
+		this.email = email;
+	}
+	
+	
+	// constructors < --- > setters
+	
+	
+	public void setNome (String nome) {
+		this.nome =  nome;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+	}
+	
+	public void setEmail (String email) {
+		this.email = email;
+	}
+
+	
+	// setters < --- > getters 
+	
+	
+	public String getNome () {
+		return this.nome;
+	}
+	
+	public int getId () {
+		return this.id;
+	}
+	
+	public String getEmail () {
+		return this.email;
+	}
+	
 
 }
 
