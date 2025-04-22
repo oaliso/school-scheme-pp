18e4c9c5a82302cc33c1e5e91612ec08423db79d
tiago-jura <tiagoar4ujo@gmail.com>
2025-04-21 22:17:47 -0400

    adicionando avaliação

---
+++ Avaliacao.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Avaliacao {
+    private String tipo; // prova, trabalho, projeto
+    private float notaMaxima;
+    private float peso;
+    private Turma turma;
+    private ArrayList<Submissao> submissoes = new ArrayList<>();
+
+    public Avaliacao(String tipo, float notaMaxima, float peso, Turma turma) {
+        this.tipo = tipo;
+        this.notaMaxima = notaMaxima;
+        this.peso = peso;
+        this.turma = turma;
+        turma.adicionarAvaliacao(this);
+    }
+
+    public void adicionarSubmissao(Submissao submissao) {
+        submissoes.add(submissao);
+    }
+
+    // Getters e Setters
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getNotaMaxima() {
+        return notaMaxima;
+    }
+
+    public void setNotaMaxima(float notaMaxima) {
+        this.notaMaxima = notaMaxima;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
+    }
+
+    public ArrayList<Submissao> getSubmissoes() {
+        return submissoes;
+    }
+}
